c96eb6ef69757af87c088e9ba1cf06eda34ca1af
Andy Stevens <apstevens@shayledevops.co.uk>
2020-09-29 20:03:12 +0100

    Added variables and functions

--- js/app.js
+++ js/app.js
@@ -3,10 +3,32 @@
 document.addEventListener('DOMContentLoaded', () => {
     const qwerty = document.getElementById('qwerty');
     const phrase = document.getElementById('phrase');
-    const start = document.getElementById('btn_reset');
+    const start = document.querySelector('.btn__reset');
     const overlay = document.getElementById('overlay');
 
     let missed = 0; // If the player guesses wrong 5 times game over.
+
+    const phrases = [
+        "Dog's dinner",
+        "Fish out of water",
+        "Chip off the old block",
+        "Bob's your uncle",
+        "Fruits of your loins"
+    ];
+
+    function getRandomPhrasesArray(arr){
+        let randomNumber = Math.floor(Math.random() * arr.length);
+        let randomPhrase = arr[randomNumber].split("");
+        return randomPhrase;
+    }
+
+    function addPhraseToDisplay(arr){
+
+    }
+
+    const phraseArray = getRandomPhrasesArray(phrases);
+
+    addPhraseToDisplay(phraseArray);
 
     start.addEventListener('click', () => {
         overlay.style.display = 'none';
