8396e9c1ea09f3c5eb0094cce0a5a61a4240f780
Andy Stevens <apstevens@shayledevops.co.uk>
2020-09-30 19:48:41 +0100

    Added conditional statements and functions

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
-// Get the elements needed from HTML
+// Add DOMContentLoaded event handler.
 
 document.addEventListener('DOMContentLoaded', () => {
+    // Get the elements needed from HTML
     const qwerty = document.getElementById('qwerty');
     const phrase = document.getElementById('phrase');
     const start = document.querySelector('.btn__reset');
     const overlay = document.getElementById('overlay');
+    const ul = document.querySelector('#phrase ul');
 
     let missed = 0; // If the player guesses wrong 5 times game over.
 
@@ -16,19 +18,94 @@ document.addEventListener('DOMContentLoaded', () => {
         "Fruits of your loins"
     ];
 
+    // Gets a random phrase
+
     function getRandomPhrasesArray(arr){
         let randomNumber = Math.floor(Math.random() * arr.length);
         let randomPhrase = arr[randomNumber].split("");
         return randomPhrase;
     }
 
-    function addPhraseToDisplay(arr){
+    // Adds letters from phrases to display
 
+    function addPhraseToDisplay(arr){
+        for (let i = 0; i < arr.length; i++) {
+            const li = document.createElement('li');
+            li.textContent = arr[i];
+            ul.appendChild(li);
+
+            if (arr[i] !== ' ') {
+                li.classList.add('letter');
+            } else {
+                li.classList.add('space');
+            }
+        }
     }
 
     const phraseArray = getRandomPhrasesArray(phrases);
 
     addPhraseToDisplay(phraseArray);
+
+    // Function checks if a letter is in the phrase
+
+    function checkLetter(btn) {
+        let phraseItem = ul.children;
+        let correct = null;
+
+        for (let i = 0; i < phraseItem.length; i++) {
+            const character = phraseItem[i].textContent.toLowerCase();
+
+            if (btn.textContent === character){
+                phraseItem[i].classList.add('show');
+            }
+        }
+        return correct;
+    }
+
+    checkLetter(qwerty);
+    
+    //Event handler listening for the in game keyboard clicks 
+
+    qwerty.addEventListener('click', e => {
+        const select = e.target;
+        if (select.tagName === 'BUTTON') {
+            select.classList.add('chosen');
+            select.setAttribute('disabled', true);
+
+            const matched = checkLetter(select);
+
+            if (!matched) {
+                const numberOfTries = document.querySelectorAll('.tries');
+                numberOfTries[missed].style.display = 'none';
+                missed++;
+            }
+            if (select) {
+                return checkWin(select);
+            }
+        }
+    });
+
+    // Function to check if game has been won or lost
+
+    const checkWin = () => {
+        const letter = ul.getElementsByClassName('letter');
+        const show = ul.getElementsByClassName('show');
+
+        if (letter.length === show.length){
+            overlay.classList.add('win');
+            overlay.querySelector('h2').textContent = "Congratulations! You've won!";
+            overlay.style.display = 'flex';
+        }
+        if ( missed > 4) {
+            overlay.classList.add('lose');
+            overlay.querySelector('h2').textContent = "Oops that's the wrong answer, you lose! Better luck next time :)";
+            overlay.style.display = 'flex';
+            overlay.querySelector('a').textContent = 'Play Again?';
+            resetGame();
+        }
+    }
+
+    // Event handler to listen for the click of the start button.
 
     start.addEventListener('click', () => {
         overlay.style.display = 'none';
